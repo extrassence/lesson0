fc4eed1b765eb061b5f544073fad07d7d644aef4
extrassence <extrassence@yandex.ru>
2024-07-20 21:39:43 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+homeworkCount=12
+totalHomeworkTime=1.5
+nameOfCourse="Python"
+avgHomeworkTime=totalHomeworkTime/homeworkCount
+print("Количество выполненных ДЗ:",homeworkCount)
+print("Количество затраченных часов:",totalHomeworkTime)
+print("Название курса:",nameOfCourse)
+print("Время на одно задание:",avgHomeworkTime)
---
+++ homework3.py
@@ -0,0 +1,8 @@
+name="Роман"
+age=45
+print("Имя:",name)
+print("Возраст:",age)
+age=age+1
+is_retiree = age>=60 #немножко усложнил для интереса
+print("Новый возраст:",age)
+print("Пенсионер:",is_retiree)
