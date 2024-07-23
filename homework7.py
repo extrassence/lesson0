cc124eb6651755e9df4589324507cc58e6640e41
extrassence <extrassence@yandex.ru>
2024-07-23 23:31:57 +0300

    Add files via upload

---
+++ homework7.py
@@ -0,0 +1,7 @@
+students = {'Ivanova', 'Znaykina', 'Botanov', 'Petrov', 'Vasechkin', 'Lentyaev', 'Balbesov', 'Sklerozov',
+            'Progulshikov'}
+grades = [[4, 3, 4, 3, 2, 3], [5, 5, 5, 5, 5, 5, 5], [5, 5, 4, 5, 5, 4], [4, 3, 2, 3, 5, 3, 2], [4, 5, 3, 5, 4],
+          [1, 2, 1, 3, 2, 3], [2, 3, 4, 3, 2, 2, 4], [4, 4, 3, 5, 5, 4], [5, 5, 5, 5, 4, 5]]
+# пришлось полистать документацию в поисках способа сделать словарь из двух списков
+journal = {sorted(list(students))[i]: int(sum(grades[i]) / len(grades[i])*100) / 100 for i in range(len(students))}
+print(journal)
