be52b38dfc5004fbb882fe78fe3b1b92d44971f2
extrassence <extrassence@yandex.ru>
2024-07-31 22:46:38 +0300

    Update homework22.py
    Убрал знак равенства в условии цикла

--- homework22.py
+++ homework22.py
@@ -1,6 +1,6 @@
 i = 0
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-while i <= len(my_list):
+while i < len(my_list):
     if my_list[i] > 0:
         print (my_list[i])
     elif my_list[i] < 0:
