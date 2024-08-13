b237d5ba01bd009534f75df334ea2ab42c546af3
extrassence <extrassence@yandex.ru>
2024-08-13 11:28:55 +0300

    Add files via upload

---
+++ homework41.py
@@ -0,0 +1,12 @@
+from homework41fake import divide as fake_divide
+from homework41true import divide as true_divide
+
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ homework41fake.py
@@ -0,0 +1,5 @@
+def divide(a, b):
+    if b == 0:
+        return ('Error')
+    else:
+        return a / b
